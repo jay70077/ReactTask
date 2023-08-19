024679d6d253c9658461c4cfe12d0f2526f09c7f
jaydeep-webonise <jaydeep.vishwakarma@weboniselab.com>
2023-08-19 20:11:07 +0530

    Updates

---
+++ precache-manifest.b7abaf565580de76b3d701ad0dd0fa17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a2ab5f9754a245c9150cfe9e7233d19",
+    "url": "/ReactTask/index.html"
+  },
+  {
+    "revision": "42103fdd1f462bfd4cbc",
+    "url": "/ReactTask/static/css/main.c56d17f6.chunk.css"
+  },
+  {
+    "revision": "b1b3cb87a56ac0165555",
+    "url": "/ReactTask/static/js/2.70c451d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTask/static/js/2.70c451d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42103fdd1f462bfd4cbc",
+    "url": "/ReactTask/static/js/main.a1aaafe8.chunk.js"
+  },
+  {
+    "revision": "3ef9380e907d893b139b",
+    "url": "/ReactTask/static/js/runtime-main.4a6e33dd.js"
+  }
+]);
